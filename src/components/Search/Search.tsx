932061169cf504e859e467ec53d5cc247f540097
Boris Mujkovic <boris.mujkovic@dataart.com>
2023-11-16 14:07:54 +0100

    Added ReactAutocomplete

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,10 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from "./Search.module.css";
+import Autocomplete from "react-autocomplete";
+import { cities } from '../../api/api';
+import { Link } from 'react-router-dom';
 
 const Search = () => {
+  const [search, setSearch] = useState("");
+  const [city, setCity] = useState(cities); 
+
+  const handleSelect = (selectedValue: string) => {
+    setSearch(selectedValue);
+};
+
+useEffect(() => {
+  const filteredCities = cities.filter(city => city.city.toLowerCase().includes(search.toLowerCase()))
+  setCity(filteredCities);
+}, [search]);
+
   return (
     <div className={styles.search}>
-      <input type="text" placeholder='Search...' />
+      <Autocomplete
+                    inputProps={{
+                        placeholder: "Search",
+                    }}
+                    value={search}
+                    items={
+                        search ? city : []
+                    }
+                    getItemValue={(item) => item.city}
+                    renderItem={(item, isHighlighted: boolean) => (
+                        <Link to={`/${item.id}`} key={item.id}>
+                            <div
+                                key={item.id}
+                                style={{
+                                    background: isHighlighted ? "lightgray" : "white",
+                                    padding: "10px",
+                                    fontWeight: "700",
+                                    textDecoration: "none",
+                                    color: "black",
+                                    cursor: "pointer",
+                                    borderBottom: "1px solid #282c34",
+                                }}
+                            >
+                                {item.city || []}
+                            </div>
+                        </Link>
+                    )}
+                    onChange={(e) => setSearch(e.target.value)}
+                    onSelect={handleSelect}
+                    menuStyle={{
+                        position: "fixed",
+                        backgroundColor: "black",
+                        zIndex: 9999,
+                        maxHeight: "200px",
+                        overflow: "auto",
+                    }}
+                />
     </div>
   )
 }
